3cb571a6d5f3310aa197b5e6821c0bf3e19ee6e3
zile028@gmail.com <zile028@gmail.com>
2021-03-10 22:35:12 +0100

    first commit

---
+++ function.js
@@ -0,0 +1,11 @@
+function prvaFunkcija() {
+  alert("Zdravo svete!");
+}
+
+function drugaFunkcija(poruka) {
+  alert(poruka);
+}
+
+function trecaFunkcija(poruka) {
+  document.getElementById("rezultat").innerHTML = poruka;
+}
